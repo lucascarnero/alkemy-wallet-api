ec46749d2b2b553d12947ec708fb64fe07c78e7c
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-25 07:19:40 -0300

    refactor(users): agrega middleware a rutas protegidas

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,13 @@ const {
   update,
   remove,
 } = require("../controllers/users");
+const { isAuthenticated } = require("../middlewares/isAuthenticated");
+const { isAdmin } = require("../middlewares/isAdmin");
 
-router.get("/", getAll);
+router.get("/", isAuthenticated, isAdmin, getAll);
 router.post("/", insert);
 router.get("/:id", getById);
 router.put("/:id", update);
-router.delete("/:id", remove);
+router.delete("/:id", isAuthenticated, isAdmin, remove);
 
 module.exports = router;
