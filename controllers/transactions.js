7f46c8acc469bb5333912f32033d5697100b5939
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-25 16:24:54 -0300

    feat(transactions): agrega paginado a endpoint de listado de items

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,9 +1,30 @@
 const { Transaction: Model } = require("../models");
+const { ITEMS_PER_PAGE } = process.env;
 
 const getAll = async (req, res) => {
   try {
-    const entities = await Model.findAll();
-    return res.status(200).json(entities);
+    const total = await Model.count();
+
+    let page = req.query.page ? Number(req.query.page) : 1;
+    if (page <= 0) page = 1;
+
+    const offset = (page - 1) * ITEMS_PER_PAGE;
+
+    const entities = await Model.findAll({
+      limit: Number(ITEMS_PER_PAGE),
+      offset,
+    });
+
+    const response = {
+      previousPage: page == 1 ? null : `/transactions/?page=${page - 1}`,
+      nextPage:
+        total > page * ITEMS_PER_PAGE
+          ? `/transactions/?page=${page + 1}`
+          : null,
+      data: entities,
+    };
+
+    return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -29,15 +50,35 @@ const getById = async (req, res) => {
 const getByUserId = async (req, res) => {
   try {
     const { userId } = req.user;
+    const total = await Model.count({
+      where: {
+        userId,
+      },
+    });
 
+    let page = req.query.page ? Number(req.query.page) : 1;
+    if (page <= 0) page = 1;
+
+    const offset = (page - 1) * ITEMS_PER_PAGE;
     const entities = await Model.findAll({
       where: {
         userId,
       },
+      limit: Number(ITEMS_PER_PAGE),
+      offset,
       order: [["date", "DESC"]],
     });
 
-    return res.status(200).json(entities);
+    const response = {
+      previousPage: page == 1 ? null : `/transactions/?page=${page - 1}`,
+      nextPage:
+        total > page * ITEMS_PER_PAGE
+          ? `/transactions/?page=${page + 1}`
+          : null,
+      data: entities,
+    };
+
+    return res.status(200).json(response);
   } catch (error) {
     return res.status(500).json(error);
   }
