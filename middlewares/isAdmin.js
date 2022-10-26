86b1890b0c5406bf190c47387db213315fedfa38
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-26 08:38:57 -0300

    feat(isAdmin.middleware): remueve depuracion por consola

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,7 +1,6 @@
 const { ROLE_ADMIN_ID } = process.env;
 
 const isAdmin = (req, res, next) => {
-  console.log(req.user);
   const { roleId } = req.user;
 
   if (roleId != ROLE_ADMIN_ID) return res.sendStatus(403);
