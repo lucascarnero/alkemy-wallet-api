86a488849c548009bb02418a4d877bbf7b4baba2
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-26 01:29:55 -0300

    feat(auth): mejora la gestion de errores en servicio de autenticacion

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,35 +1,44 @@
 const bcrypt = require("bcrypt");
 const { generateToken } = require("../helpers/auth");
 const { User } = require("../models");
+const CustomError = require("../helpers/customerror");
 
-const login = async (req, res) => {
-  const { email, password } = req.body;
+const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne({
-    where: {
-      email,
-    },
-  });
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
 
-  if (!user) return res.sendStatus(401);
+    if (!user) throw new CustomError("No autorizado", 401);
 
-  bcrypt.compare(password, user.password, function (error, result) {
-    if (error || !result) return res.sendStatus(401);
+    bcrypt.compare(password, user.password, function (error, result) {
+      if (error || !result) throw new CustomError("No autorizado", 401);
 
-    const { id, roleId } = user;
-    const accessToken = generateToken({ userId: id, roleId });
-    return res.status(200).json({ accessToken });
-  });
+      const { id, roleId } = user;
+      const accessToken = generateToken({ userId: id, roleId });
+      return res.status(200).json({ accessToken });
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
-const me = async (req, res) => {
-  const { userId } = req.user;
+const me = async (req, res, next) => {
+  try {
+    const { userId } = req.user;
 
-  const user = await User.findByPk(userId);
+    const user = await User.findByPk(userId);
 
-  if (!user) return res.sendStatus(404);
+    if (!user) throw new CustomError("No encontrado", 404);
 
-  return res.status(200).json(user);
+    return res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = {
