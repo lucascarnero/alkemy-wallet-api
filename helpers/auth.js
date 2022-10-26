5d19f01091b49cc6c8e89554dede0728fb4e8832
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-26 08:37:27 -0300

    feat(auth.middleware): mejora la gestion de los errores

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 const { TOKEN_SECRET, TOKEN_EXPIRES_IN } = process.env;
+const CustomError = require("../helpers/customerror");
 
 const generateToken = ({ userId, roleId }) => {
   return jwt.sign({ data: { userId, roleId } }, TOKEN_SECRET, {
@@ -8,17 +9,21 @@ const generateToken = ({ userId, roleId }) => {
 };
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
 
-  if (token == null) return res.sendStatus(401);
+    if (token == null) throw new CustomError("Sin acceso", 401);
 
-  jwt.verify(token, TOKEN_SECRET, (error, decoded) => {
-    if (error) return res.sendStatus(403);
-    req.user = decoded.data;
+    jwt.verify(token, TOKEN_SECRET, (error, decoded) => {
+      if (error) throw new CustomError("Prohibido", 403);
+      req.user = decoded.data;
 
-    next();
-  });
+      next();
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 module.exports = {
   generateToken,
