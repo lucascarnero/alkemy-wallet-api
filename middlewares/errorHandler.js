57fe04800f553455561c4e31fb866fcb3eb04466
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-29 15:16:09 -0300

    feat(errorHandler.middleware): mejora la descripcion de los errores

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,13 @@
 const CustomError = require("../helpers/customerror");
 
 const errorHandler = (error, req, res, next) => {
+  console.log(error);
+
   if (!(error instanceof CustomError)) {
-    error = new CustomError("Error desconocido", 500);
+    if (error.parent?.sqlMessage)
+      error = new CustomError(error.parent.sqlMessage, 500);
+    else
+      error = new CustomError("Error desconocido", 500);
   }
 
   res.status(error.status).json(error);
