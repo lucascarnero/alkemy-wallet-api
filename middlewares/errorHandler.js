2b3c4fa987945e8c278f4db8590e93115c9bd56b
Lucas Carnero <lucas.carnero@naranja.com>
2022-10-30 22:08:25 -0300

    quito depuracion y agrego sequelize-cli a dependencias de proyecto

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,6 @@
 const CustomError = require("../helpers/customerror");
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error);
-
   if (!(error instanceof CustomError)) {
     if (error.parent?.sqlMessage)
       error = new CustomError(error.parent.sqlMessage, 500);
